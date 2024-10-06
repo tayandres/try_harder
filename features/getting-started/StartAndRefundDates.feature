5ac4152cfaaa5a81a0b25a5e2c747a4c9c7c91f1
tayandres <tayloralexisandres@gmail.com>
2024-10-05 18:59:03 -0600

    push

---
+++ features/getting-started/StartAndRefundDates.feature
@@ -0,0 +1,26 @@
+@sep11
+Feature: Program start dates and Refund dates
+
+    As a customer, I want to see the program start dates and refund policy details before enrolling 
+    so that I can make informed decisions.
+
+    #* AC1: Program Start date and refund dates must be displayed in Step 1 in Test Automation with Selenium Program.
+
+    
+   
+   
+   
+    #TODO: Create scenarios that cover all the acceptance criteria
+Background: 
+Given user navigates the enrollment
+
+
+Scenario: As a customer, I want to see the program start dates and refund policy details before enrolling so that I can make informed decisions.
+#Then I see the url to contain ""
+Then I see the program start date details are displayed
+Then I see the refund policy date details are displayed
+
+
+Scenario: Verify that the progman dates are correct
+Then I see the program start date is in the correct format
+Then I see the refund policy date is in the correct format
---
+++ steps/getting-started/startAndRefundDates.js
@@ -0,0 +1,34 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+import { expect} from "@playwright/test";
+import { startApplicationPage, page } from "../../globalPagesSetup.js";
+import { productInfo } from "../../utilities/qa-data-reader.js";
+
+ 
+  Given('user navigates the enrollment', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+    Then('I see the program start date details are displayed', function () {
+      // Write code here that turns the phrase above into concrete actions
+      return 'pending';
+    });
+
+    Then('I see the refund policy date details are displayed', function () {
+      // Write code here that turns the phrase above into concrete actions
+      return 'pending';
+    });
+
+
+  Then('I see the program start date is in the correct format', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
+
+  Then('I see the refund policy date is in the correct format', function () {
+    // Write code here that turns the phrase above into concrete actions
+    return 'pending';
+  });
+
